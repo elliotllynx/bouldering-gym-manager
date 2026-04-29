eb57f42727184f75809770a80e6369051aef1434
Elliot Hečová <elliot.hecova@gmail.com>
2026-04-29 02:09:02 +0200

    feat. maui template created

---
+++ Project/BoulderSetManager/BoulderSetManager/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BoulderSetManager
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
